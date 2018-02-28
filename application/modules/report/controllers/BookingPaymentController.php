a614d0b39d6efe688e2a32a1bded76f971846581
samlanh <mok_channy@yahoo.com>
2018-02-28 08:21:26 +0700

    syheng commit report

--- application/modules/report/controllers/BookingPaymentController.php
+++ application/modules/report/controllers/BookingPaymentController.php
@@ -246,5 +246,25 @@ class Report_BookingPaymentController extends Zend_Controller_Action {
 	  	Application_Model_Decorator::removeAllDecorator($frm);
 	  	$this->view->frm_search = $frm;
 	  }
+	  
+	  function rptCustomerAlertTimeAction(){
+	  	if($this->getRequest()->isPost()){
+	  		$search = $this->getRequest()->getPost();
+	  	}
+	  	else{
+	  		$search = array(
+	  				'adv_search'=>'',
+	  				'status'=>-1,
+	  				// 'start_date'=> date('Y-m-d'),
+	  				// 	  				'end_date'=>date('Y-m-d')
+	  		);
+	  	}
+	  	$db = new Report_Model_DbTable_DbBookingPayment();
+	  	$this->view->d_customer = $db->getCustomerAlertTime($search);
+	  	$frm = new Application_Form_FrmAdvanceSearch();
+	  	$form = $frm->AdvanceSearch();
+	  	Application_Model_Decorator::removeAllDecorator($form);
+	  	$this->view->frm = $form;
+	  }
 }
 
--- application/modules/report/models/DbTable/DbBookingPayment.php
+++ application/modules/report/models/DbTable/DbBookingPayment.php
@@ -334,6 +334,7 @@ class Report_Model_DbTable_DbBookingPayment extends Zend_Db_Table_Abstract
 			$where .= " AND ".$to_date;
 	      	return $db->fetchAll($sql.$where);
       }
+      
       function getCommissionPaymentById($id){
       	$db=$this->getAdapter();
       	$where =" ";
@@ -524,5 +525,56 @@ class Report_Model_DbTable_DbBookingPayment extends Zend_Db_Table_Abstract
       return $db->fetchAll($sql.$where.$order);
       }
       
+      function getCustomerAlertTime($search){
+      	$db = $this->getAdapter();
+      	$glob=new Application_Model_DbTable_DbGlobal();
+      	$lang= $glob->getCurrentLang();
+      	$array = array(1=>"name_en",2=>"name_kh");
+      	$where='';
+      	$sql=" SELECT cb.id,cb.`booking_no`,
+      	CONCAT(c.`last_name`,'(',customer_code,')') AS cus_name,
+      	l.`location_name` AS from_location,
+      	tl.`location_name` AS to_location,
+      	cb.`booking_date`,
+      	cb.`delivey_date`,
+      	cb.*,
+      	cb.`price`,cb.`commision_fee`,cb.`other_fee`,cb.`total`,
+      	(SELECT CONCAT(d.`first_name`,' ',d.`last_name`) FROM `ldc_driver` AS d WHERE d.`id` = cb.`driver_id` LIMIT 1) AS driver,cb.driver_fee,
+      	(SELECT $array[$lang] FROM tb_view AS v WHERE v.key_code=cb.status_working AND v.type=17 LIMIT 1) book_status,
+      	cb.`status`
+      	FROM `ldc_carbooking` AS cb,
+      	`ldc_customer` AS c,
+      	`ldc_package_location` AS l,
+      	`ldc_package_location` AS tl
+      	WHERE
+      	c.`id` = cb.`customer_id`
+      	AND l.`id` = cb.`from_location`
+      	AND tl.`id` = cb.`to_location`
+      	AND cb.`status` >-1 ";
+//       	if($search["adv_search"] !=""){
+//       	$s_where=array();
+//       	$s_search=addslashes(trim($search['adv_search']));
+//       	$s_where[]=" CONCAT(c.`last_name`,'(',customer_code,')') LIKE '%{$s_search}%'";
+//       	$s_where[]=" cb.`booking_no` LIKE '%{$s_search}%'";
+//       	$s_where[]=" tl.`location_name` LIKE '%{$s_search}%'";
+//       	$s_where[]=" l.`location_name` LIKE '%{$s_search}%'";
+//       	$s_where[]=" cb.`price` LIKE '%{$s_search}%'";
+//       	$s_where[]=" cb.`commision_fee` LIKE '%{$s_search}%'";
+//       	$s_where[]=" cb.`other_fee` LIKE '%{$s_search}%'";
+//       	$s_where[]=" cb.`total` LIKE '%{$s_search}%'";
+//       	$where.=' AND ('.implode(' OR ',$s_where).')';
+//       	}
+//       	if ($search['status']>-1){
+//       	$where .=' AND cb.`status` = '.$search['status'];
+//       }
+//       if ($search['customer']>0){
+//       $where .=' AND  cb.`customer_id` = '.$search['customer'];
+//       }
+//       $str_next = '+1 1 days';
+//       $search['end_date']=date("Y-m-d", strtotime($search['end_date'].$str_next));
+//       $to_date = (empty($search['end_date']))? '1': " cb.`delivey_date` <= '".$search['end_date']." 23:59:59'";
+//       $where .= " AND ".$to_date;
+      return $db->fetchAll($sql.$where);
+      }
  }
 
